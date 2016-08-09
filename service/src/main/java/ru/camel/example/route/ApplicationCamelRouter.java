1035e0e4ad9608bc63e4d2d5b10e4188cc8d94cf
tsval <tsvetval@gmail.com>
2016-08-09 21:50:16 +0300

    add routes

--- service/src/main/java/ru/camel/example/route/ApplicationCamelRouter.java
+++ service/src/main/java/ru/camel/example/route/ApplicationCamelRouter.java
@@ -3,10 +3,14 @@ package ru.camel.example.route;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Expression;
-import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeType;
+
+import static org.apache.camel.Exchange.CONTENT_TYPE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 /**
  *
@@ -22,8 +26,17 @@ public class ApplicationCamelRouter extends RouteBuilder {
         // .dataFormatProperty("prettyPrint", "true")
         // .port(8081);;
 
+        rest()
+            .get("/ping").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest()
+            .post("/payments").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest()
+            .post("/payments/{session}").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest()
+            .get("/payments/{session}").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest()
+            .post("/payments/{session}/cancel").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest()
+            .post("/payments/{session}/confirm").route().transform().constant("OK").setHeader(CONTENT_TYPE, constant(TEXT_PLAIN_VALUE)).endRest();
+//                .marshal().json(JsonLibrary.Jackson).convertBodyTo(String.class).setExchangePattern(ExchangePattern.RobustInOnly)
+//                .end();
 
-        from("servlet:///test")
+      /*  from("servlet:///test")
                 .onException(Exception.class).handled(true)
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
                 .setHeader(Exchange.CONTENT_TYPE, constant("text/plain")).setBody(simple("Not accepted")).end()
@@ -45,7 +58,7 @@ public class ApplicationCamelRouter extends RouteBuilder {
                 .marshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
                 //.pipeline("direct:validate", "direct:transform")
                 .setExchangePattern(ExchangePattern.RobustInOnly)
-                .end();
+                .end();*/
 
 
 //        rest("/camel")
